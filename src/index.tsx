da5cbf6f224188476088325081a93d5c5d81b14c
周正纬 <resetsix223@gmail.com>
2023-03-10 17:51:16 +0800

    安装jira-dev-tool

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { loadDevTools } from "jira-dev-tool";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+loadDevTools(() =>
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 );
