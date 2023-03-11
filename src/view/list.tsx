13302da125be1669490b517fd9b0670f7581a1c2
周正纬 <resetsix223@gmail.com>
2023-03-11 21:32:08 +0800

    添加列表选项

--- src/view/list.tsx
+++ src/view/list.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Table, TableProps } from "antd";
 import { Project } from "../types/projects";
 import { User } from "../types/users";
+import dayjs from "dayjs";
 
 interface ListType extends TableProps<Project> {
   users: User[];
@@ -13,7 +14,7 @@ export const List = ({ users, lists }: ListType) => {
     <Table
       columns={[
         {
-          title: "项目",
+          title: "名称",
           dataIndex: "name",
         },
         {
@@ -23,6 +24,22 @@ export const List = ({ users, lists }: ListType) => {
               <span>
                 {users.find((user) => user.id === project.personId)?.name ||
                   "未知"}
+              </span>
+            );
+          },
+        },
+        {
+          title: "部门",
+          dataIndex: "organization",
+        },
+        {
+          title: "创建时间",
+          render(value, project) {
+            return (
+              <span>
+                {project.created
+                  ? dayjs(project.created).format("YYYY-MM-DD")
+                  : null}
               </span>
             );
           },
