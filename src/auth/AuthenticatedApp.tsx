9978653ed8b6efcef9bffb550fb29a7191cf73b0
周正纬 <resetsix223@gmail.com>
2023-03-12 21:53:01 +0800

    闭包原理与修改文档标题

--- src/auth/AuthenticatedApp.tsx
+++ src/auth/AuthenticatedApp.tsx
@@ -5,7 +5,7 @@ import { ProjectList } from "../view";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
-  useTitle('项目列表',false)
+  useTitle("项目列表", false);
   return (
     <div>
       <Button onClick={() => logout()}>退出</Button>
--- src/hooks/useTitle.ts
+++ src/hooks/useTitle.ts
@@ -1,17 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export const useTitle = (title: string, keepOnUnmount = true) => {
-  const oldTitle = title;
+  // useRef方法的current属性的值，在整个生命周期中都不会改变。用于保存原始标题的引用
+  const oldTitle = useRef(document.title).current;
 
   useEffect(() => {
+    // 将文档title修改为传入的指定title
     document.title = title;
   }, [title]);
 
   useEffect(() => {
     return () => {
       if (!keepOnUnmount) {
+        // 当组件组件卸载时，将文档title修改为原来最初的title
         document.title = oldTitle;
       }
     };
-  }, []);
+  }, [keepOnUnmount, oldTitle]);
 };
