d9fef96e29e1d54171387f8c94520095dc0e66e6
一闪一闪亮晶晶 <460730259@qq.com>
2023-03-14 15:24:35 +0800

    清除warning

--- src/hooks/useMount.ts
+++ src/hooks/useMount.ts
@@ -3,5 +3,6 @@ import { useEffect } from "react";
 export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
